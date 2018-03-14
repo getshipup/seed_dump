874cf1f362570dc463a3c5bf5adf1fe98e01d0fa
quentin <q.maurice@hotmail.fr>
2018-03-14 16:20:10 +0100

    started by company in the check

--- lib/seed_dump/environment.rb
+++ lib/seed_dump/environment.rb
@@ -11,10 +11,10 @@ class SeedDump
       models.each do |model|
         model = model.limit(limit) if limit.present?
 
-        if model.column_names.include?('company_id')
-          model = model.where('company_id < 10 OR company_id % 20 = 0')
-        elsif model == Company
+        if model == Company
           model = model.where('id < 10 OR id % 20 = 0')
+        elsif model.column_names.include?('company_id')
+          model = model.where('company_id < 10 OR company_id % 20 = 0')
         end
 
         SeedDump.dump(model,
